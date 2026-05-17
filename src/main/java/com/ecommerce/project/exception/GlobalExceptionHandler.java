048742bf075e9849ceafe78507c029631f0ae190
Baontin <tinvtpd07947@gmail.com>
2026-05-17 18:03:11 +0700

    adding a global exption handler that caught and handle MethodArgumentNotValid exeption

--- src/main/java/com/ecommerce/project/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exception/GlobalExceptionHandler.java
@@ -1,4 +1,30 @@
 package com.ecommerce.project.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex
+    ) {
+        Map<String, String> response = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+            response.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
