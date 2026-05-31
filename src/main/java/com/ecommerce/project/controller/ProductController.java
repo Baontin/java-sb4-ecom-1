8f935c4b31352e6f4443993ded0117bcd3dd2cbc
Baontin <tinvtpd07947@gmail.com>
2026-05-31 08:01:04 +0700

    feat(backend): implement product module, add create, update, get functions for product.

---
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -0,0 +1,55 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+import com.ecommerce.project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody Product product,
+                                                    @PathVariable Long categoryId) {
+
+        ProductDTO productDTO = productService.createProduct(categoryId, product);
+        return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/public/products")
+    public ResponseEntity<ProductResponse> getAllProducts() {
+
+        ProductResponse products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/public/categories/{categoryId}/products")
+    public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId) {
+
+        ProductResponse products = productService.getProductsByCategory(categoryId);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/public/products/keyword/{keyword}")
+    public ResponseEntity<ProductResponse> getProductsByKeyword(@PathVariable String keyword) {
+
+        ProductResponse products = productService.getProductsByKeyword(keyword);
+        return new ResponseEntity<>(products, HttpStatus.FOUND);
+    }
+
+    @PutMapping("/admin/product/{productId}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long productId,
+                                                    @RequestBody Product product) {
+
+        ProductDTO productDTO = productService.updateProduct(product, productId);
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
+    }
+}
