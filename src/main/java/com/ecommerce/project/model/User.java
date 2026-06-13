210ac586be91493b5b616bcd43c50291232b4359
Baontin <tinvtpd07947@gmail.com>
2026-06-13 09:48:16 +0700

    feat(backend): Implement User, Roles, Address entities.

---
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -0,0 +1,73 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users",
+        uniqueConstraints = {
+            @UniqueConstraint(name = "uc_users_username", columnNames = "username"),
+            @UniqueConstraint(name = "uc_users_email", columnNames = "email")
+        })
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    // Unidirectional relationship (Role don't need to know User)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+                fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+        /*
+    Remove product from user’s product list:
+    - orphanRemoval = true → product deleted from DB
+    - CascadeType.REMOVE → product still stays on DB
+    * */
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user",
+                cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+                orphanRemoval = true)
+    private Set<Product> products = new HashSet<>();
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "user_address",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "address_id"))
+    private List<Address> addresses = new ArrayList<>();
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+
+}
